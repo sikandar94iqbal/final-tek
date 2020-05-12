fef5be79a40faa857fa676b43b395febf8ae1dcf
Sikandar Iqbal <sikandar.iqbal@ibm.com>
2020-05-12 14:58:18 +0500

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("HEllo from Appsodyy Demoo! to Hell0 from Cloud Paks!!");
+  res.send("HEllo from Appsodyy Demoo!! to Hell0 from Cloud Paks!!");
 });
  
 module.exports.app = app;
